eae0382e6210905ec58ed35581b3a5488d2e8960
GhostriderDK <149684275+GhostriderDK@users.noreply.github.com>
2024-03-12 11:38:14 +0100

    fixed

--- AzureVM/flask_app/app.py
+++ AzureVM/flask_app/app.py
@@ -66,7 +66,7 @@ def bat_stat_1():
     # plot
     ax = fig.subplots()
     ax.pie(x, radius=3, center=(4, 4),
-           wedgeprops={"linewidth": 1, "edgecolor": "white"}, frame=False, labels=['brugt', 'tilbage'],
+           wedgeprops={"linewidth": 1, "edgecolor": "white"}, frame=False, labels=['tilbage', 'brugt'],
        colors=['Green', 'Red'])
 
     ax.set(xlim=(0, 8), xticks=(0,8),
