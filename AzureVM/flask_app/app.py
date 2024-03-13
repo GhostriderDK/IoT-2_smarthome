3eabbd63d06cdf3415562c4deea9586ff98dd387
GhostriderDK <149684275+GhostriderDK@users.noreply.github.com>
2024-03-13 14:07:29 +0100

    fig size corrected

--- AzureVM/flask_app/app.py
+++ AzureVM/flask_app/app.py
@@ -91,7 +91,7 @@ def bat_stat():
     return data
 
 def humidity_realtime():
-    fig = Figure(figsize=(2,4))
+    fig = Figure(figsize=(3,5))
     measurement = 45
     hum1 = [measurement]
     hum2 = [measurement]
