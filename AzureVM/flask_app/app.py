de56db2f4263e7e6162616018833616c27c50a9c
GhostriderDK <149684275+GhostriderDK@users.noreply.github.com>
2024-03-15 13:47:39 +0100

    -

--- AzureVM/flask_app/app.py
+++ AzureVM/flask_app/app.py
@@ -51,7 +51,7 @@ def stue_data_co2_tvoc_part():
     fig.subplots_adjust(bottom=0.3)
     ax1.set_facecolor("white")
     ax1.plot(timestamps, tvoc, linestyle="dashed", c="#11f", linewidth="1.5", marker="d")
-    ax1.set_ylabel("Total volatile organic compunds")
+    ax1.set_ylabel("TVOC in ppb")
     ax1.tick_params(axis="y", colors="blue")
     ax1.spines["left"].set_color("blue")
     ax1.set_xticks([])  
