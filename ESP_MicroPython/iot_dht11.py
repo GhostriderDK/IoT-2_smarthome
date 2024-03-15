3a23d8a6b90d7e32b2057b58c4a84262cb191f8f
jacobrsv <j@cobs.dk>
2024-03-15 09:49:32 +0100

    dht11 done

--- ESP_MicroPython/iot_dht11.py
+++ ESP_MicroPython/iot_dht11.py
@@ -3,11 +3,13 @@ from utime import sleep
 import asyncio
 from machine import I2C, Pin
 
+
 ####  Sensors  ####
 import dht                             # DHT11
 pin_dht11 = 32
 sensor = dht.DHT11(Pin(pin_dht11))
 # Battery
+from adc_sub import ADC_substitute
 pin_battery = 34                       # ADC1_6
 batttery = ADC_substitute(pin_battery)
 
@@ -23,6 +25,9 @@ led = Pin(23, Pin.OUT)
 config['server'] = '52.236.38.161'  # Change to suit
 config['ssid'] = 'iot'
 config['wifi_pw'] = 'gruppe06'
+
+def get_battery_percentage():          # The battery voltage percentage
+    return 24
 
 def reboot_esp():
     print("Rebooting!\n\n")
@@ -45,23 +50,26 @@ async def main(client):
     except:
         print('Error')
         reboot_esp()
-
+    
     for coroutine in (up, messages):
         asyncio.create_task(coroutine(client))
 
     while True:
         await asyncio.sleep(10)
         led.on()
-
+        
         #print('Publish', scd40_data)
         bat_pct = get_battery_percentage()
-
+        
         # Check the sensor
         sensor.measure()
         sensor_value_temp = sensor.temperature()
         sensor_value_hum = sensor.humidity()
-
-        await client.publish('sensor/dht11/temp', str(sensor_value_temp), qos = 1)
+        print('Reading: ', sensor_value_temp, "C  ", sensor_value_hum, "%  ", bat_pct, "%")
+        
+        await client.publish('sensor/bedroom/temp', str(sensor_value_temp), qos = 1)
+        await client.publish('sensor/bedroom/hum', str(sensor_value_hum), qos = 1)
+        await client.publish('sensor/bedroom/bat', str(bat_pct), qos = 1)
         led.off()
 
 
@@ -73,5 +81,3 @@ try:
     asyncio.run(main(client))
 finally:
     client.close()  # Prevent LmacRxBlk:1 errors
-
-
