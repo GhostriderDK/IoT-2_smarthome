8881948b118b9f0ff924f36bca5e1efa1f1c1cd6
GhostriderDK <149684275+GhostriderDK@users.noreply.github.com>
2024-03-20 17:04:21 +0100

    tick position on x_axis

--- AzureVM/flask_app/app.py
+++ AzureVM/flask_app/app.py
@@ -22,7 +22,8 @@ def stue_temp():
     ax1.set_ylabel("Temp in C")
     ax1.tick_params(axis="y", colors="blue")
     ax1.spines["left"].set_color("blue")
-    ax1.set_xticks(range(0, len(timestamps), len(timestamps) // 20)) 
+    tick_positions = range(0, len(timestamps), len(timestamps) // 20)  
+    ax1.set_xticks(tick_positions) 
     ax1.set_xticklabels([])
     ax1.grid(axis='y', linestyle='--')
 
@@ -34,7 +35,8 @@ def stue_temp():
     ax2.set_ylabel("Humidity in %")
     ax2.tick_params(axis="x", colors="black")
     ax2.tick_params(axis="y", colors="blue")
-    ax2.set_xticks(range(0, len(timestamps), len(timestamps) // 20))
+    tick_positions = range(0, len(timestamps), len(timestamps) // 20)
+    ax2.set_xticks(tick_positions)
     ax2.spines["left"].set_color("blue")
     ax2.grid(axis='y', linestyle='--')
     
@@ -56,7 +58,8 @@ def stue_data_co2_tvoc_part():
     ax1.set_ylabel("TVOC in ppb")
     ax1.tick_params(axis="y", colors="blue")
     ax1.spines["left"].set_color("blue")
-    ax1.set_xticks(range(0, len(timestamps), len(timestamps) // 20))
+    tick_positions = range(0, len(timestamps), len(timestamps) // 20)
+    ax1.set_xticks(tick_positions)
     ax1.set_xticklabels([])
     ax1.grid(axis='y', linestyle='--')
 
@@ -69,7 +72,8 @@ def stue_data_co2_tvoc_part():
     ax2.tick_params(axis="x", colors="black")
     ax2.tick_params(axis="y", colors="blue")
     ax2.spines["left"].set_color("blue")
-    ax2.set_xticks(range(0, len(timestamps), len(timestamps) // 20))
+    tick_positions = range(0, len(timestamps), len(timestamps) // 20)
+    ax2.set_xticks(tick_positions)
     ax2.grid(axis='y', linestyle='--')
     fig.patch.set_facecolor("orange")
         
@@ -93,7 +97,8 @@ def part_in_air():
     ax.tick_params(axis="x", colors="black")
     ax.tick_params(axis="y", colors="blue")
     ax.spines["left"].set_color("blue")
-    ax.set_xticks(range(0, len(timestamps), len(timestamps) // 20))
+    tick_positions = range(0, len(timestamps), len(timestamps) // 20)
+    ax.set_xticks(tick_positions)
     ax.grid(axis='y', linestyle='--')
     fig.patch.set_facecolor("orange")
     
