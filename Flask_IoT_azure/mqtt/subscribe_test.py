b00e61cd933b2f5021a1e2a6537dc1c7f008ecdd
GhostriderDK <149684275+GhostriderDK@users.noreply.github.com>
2024-03-22 09:57:27 +0100

    Delete Flask_IoT_azure directory

--- Flask_IoT_azure/mqtt/subscribe_test.py
+++
@@ -1,8 +0,0 @@
-import paho.mqtt.subscribe as subscribe
-
-print("subscribe mqtt script running")
-
-def on_message_print(client, userdata, message):
-    print("%s %s" % (message.topic, message.payload))
-
-subscribe.callback(on_message_print, "paho/test/topic", hostname="74.234.16.65", userdata={"message_count": 0})
