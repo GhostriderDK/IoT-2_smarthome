50d60785dae0c63007dfefac07379a1cf01ad842
GhostriderDK <149684275+GhostriderDK@users.noreply.github.com>
2024-04-15 13:31:59 +0200

    20

--- AzureVM/flask_app/app.py
+++ AzureVM/flask_app/app.py
@@ -9,7 +9,7 @@ import paho.mqtt.publish as publish
 app = Flask(__name__)
 app.run(debug=True)
 datapoints = 1000
-num_ticks = 
+num_ticks = 20
 
 def bath_temp():
     timestamps, temp, hum, bat = get_bath_data(datapoints)
