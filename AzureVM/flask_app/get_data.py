49865b79fcbe6bb68174dfc75517731d7b0758ac
GhostriderDK <149684275+GhostriderDK@users.noreply.github.com>
2024-04-15 16:00:49 +0200

    removal of loop

--- AzureVM/flask_app/get_data.py
+++ AzureVM/flask_app/get_data.py
@@ -1,41 +1,40 @@
 import sqlite3
 
 def get_stue_data(number_of_rows):
-    while True:
-        query = """SELECT * FROM stue ORDER BY datetime DESC;"""
-        datetimes = []
-        temperatures = []
-        humidities = []
-        tvoc = []
-        particles =  []
-        co2 = []
-        try:
-            conn = sqlite3.connect("database/data.db")
-            cur = conn.cursor()
-            cur.execute(query)
-            rows = cur.fetchmany(number_of_rows)
-            for row in reversed(rows):
-                datetimes.append(row[0])
-                temperatures.append(row[1])
-                humidities.append(row[2]) 
-                tvoc.append(row[3])
-                particles.append(row[4])
-                co2.append(row[5])
-            print(datetimes)
-            print(temperatures)
-            print(humidities)
-            print(tvoc)
-            print(particles)
-            print(co2)
-            return datetimes, temperatures, humidities, tvoc, particles, co2         
-        except sqlite3.Error as sql_e:
-            print(f"sqlite error occurred: {sql_e}")
-            conn.rollback()
+    query = """SELECT * FROM stue ORDER BY datetime DESC;"""
+    datetimes = []
+    temperatures = []
+    humidities = []
+    tvoc = []
+    particles =  []
+    co2 = []
+    try:
+        conn = sqlite3.connect("database/data.db")
+        cur = conn.cursor()
+        cur.execute(query)
+        rows = cur.fetchmany(number_of_rows)
+        for row in reversed(rows):
+            datetimes.append(row[0])
+            temperatures.append(row[1])
+            humidities.append(row[2]) 
+            tvoc.append(row[3])
+            particles.append(row[4])
+            co2.append(row[5])
+        print(datetimes)
+        print(temperatures)
+        print(humidities)
+        print(tvoc)
+        print(particles)
+        print(co2)
+        return datetimes, temperatures, humidities, tvoc, particles, co2         
+    except sqlite3.Error as sql_e:
+        print(f"sqlite error occurred: {sql_e}")
+        conn.rollback()
 
-        except Exception as e:
-            print(f"Another error occured: {e}")
-        finally:
-            conn.close()
+    except Exception as e:
+        print(f"Another error occured: {e}")
+    finally:
+        conn.close()
 
 
 get_stue_data(1000)
