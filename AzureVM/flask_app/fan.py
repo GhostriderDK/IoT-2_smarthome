1385d7ba7b74d7451c14a12eb27a5e84b3faf45e
GhostriderDK <149684275+GhostriderDK@users.noreply.github.com>
2024-04-15 10:27:22 +0200

    elif

--- AzureVM/flask_app/fan.py
+++ AzureVM/flask_app/fan.py
@@ -4,10 +4,16 @@ from time import sleep
 
 def fan():
     timestamps, temp, hum, tvoc, part, co2 = get_stue_data(1)
-    if (temp[0] > 25 and hum[0] > 50) or (temp[0] > 25 and co2[0] > 1100):
+    if temp[0] > 25 and hum[0] > 50:
+        publish.single("sensor/stue/fan", "1", hostname="localhost")
+   
+    elif temp[0] > 25 and co2[0] > 1100:
         publish.single("sensor/stue/fan", "1", hostname="localhost")
 
-    elif (temp[0] < 18 and hum[0] < 30) or (temp[0] < 18 and co2 < 500):
+    elif  temp[0] < 18 and co2 < 500:
+        publish.single("sensor/stue/fan", "0", hostname="localhost")
+
+    elif temp[0] < 18 and hum[0] < 30:
         publish.single("sensor/stue/fan", "0", hostname="localhost")
 
 print('fan script running')
